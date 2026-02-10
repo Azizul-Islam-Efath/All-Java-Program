bdd8984ba8bce32f9e9a4fab6976ce45eab389cb
Azizul Islam <azizulislam5360@gmail.com>
2026-02-11 01:01:17 +0600

    Add files via upload

--- Java_Practise/Design_Pattern/Level2/Constructor/Product.java
+++ Java_Practise/Design_Pattern/Level2/Constructor/Product.java
@@ -1,1 +1,33 @@
+package Level2.Constructor;
 
+public class Product {
+    public String ProductId,ProductName;
+    public double Price;
+
+    public Product(String ProductId,String ProductName,double Price){
+        this.ProductId=ProductId;
+        this.ProductName=ProductName;
+        this.Price=Price;
+    }
+
+    public void Display(){
+
+        System.out.println("\nProduct ID: "+ProductId);
+        System.out.println("Product Name: "+ProductName);
+        System.out.println("Product Price: "+Price);
+    }
+
+    public void display(String ProductId,String ProductName,double Price){
+        System.out.println("\nProduct ID: "+ProductId);
+        System.out.println("Product Name: "+ProductName);
+        System.out.println("Product Price: "+Price);
+    }
+
+    public static void main(String[] args) {
+        Product p1 = new Product("123","Hand Wash",500);
+        p1.Display();
+        p1.display("154","Cleansure",120);
+
+    }
+    
+}
