0a1266071f77c5aadb1b5a083782d5c3761b1aa7
Azizul Islam <azizulislam5360@gmail.com>
2026-02-17 00:08:23 +0600

    Add files via upload

--- Java_Practise/Design_Pattern/Level3/Abstraction/Shape.java
+++ Java_Practise/Design_Pattern/Level3/Abstraction/Shape.java
@@ -1,1 +1,27 @@
+package Level3.Abstraction;
+
+public abstract class Shape {
+  public abstract void info();
+}
+
+class Rectangle extends Shape{
+    public void info(){
+        System.out.println("This is a Rectangle Shape.");
+    }
+}
+
+class Circle extends Shape{    
+    public void info(){
+        System.out.println("This a Circle Shape");
+    }
+
+
+    public static void main(String[] args) {
+        Shape R = new Rectangle();
+        R.info();
+        
+        Shape C = new Circle();
+        C.info();
+        }
+}
 
