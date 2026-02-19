584966346023c1bc1cd370d6dbd284a5ae0f9bf8
Azizul Islam <azizulislam5360@gmail.com>
2026-02-19 13:57:50 +0600

    Add files via upload

--- Java_Practise/Design_Pattern/Level3/Inheritance/Problem3/Shape.java
+++ Java_Practise/Design_Pattern/Level3/Inheritance/Problem3/Shape.java
@@ -1,1 +1,28 @@
+package Level3.Inheritance.Problem3;
 
+public class Shape {
+    public void display(){
+        System.out.println("This is a Shape.");
+    }    
+}
+
+class Circle extends Shape{
+    public void display1(){
+        System.out.println("Making Circle Through Shapes.");
+    }
+}
+class Triangle extends Shape{
+    public void display2(){
+        System.out.println("Making Triangle Through Shapes.");
+    }
+
+    public static void main(String[] args) {
+        Triangle s = new Triangle();
+        s.display();
+        s.display2();
+
+        Circle c = new Circle();
+        c.display();
+        c.display1();
+    }
+}
