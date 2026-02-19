4a8a64bb379c36d2b1080df70de1a278173b4dd5
Azizul Islam <azizulislam5360@gmail.com>
2026-02-19 13:55:45 +0600

    Add files via upload

--- Java_Practise/Design_Pattern/Level3/Inheritance/Problem1/Car.java
+++ Java_Practise/Design_Pattern/Level3/Inheritance/Problem1/Car.java
@@ -1,1 +1,21 @@
+package Level3.Inheritance.Problem1;
 
+public class Car extends Vehicle{
+
+    void Display(){
+        System.out.println("I am inside the Car class.");
+    }
+
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.display();
+        c.Display();
+    }
+
+    
+}
+class Vehicle {
+    public void display(){
+    System.out.println("I am Inside The Vehicle Class");
+    }
+}
