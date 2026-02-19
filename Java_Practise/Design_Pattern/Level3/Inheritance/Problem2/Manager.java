5385721a784ac2ee47a50b928d12045bcb75bf06
Azizul Islam <azizulislam5360@gmail.com>
2026-02-19 13:56:43 +0600

    Add files via upload

--- Java_Practise/Design_Pattern/Level3/Inheritance/Problem2/Manager.java
+++ Java_Practise/Design_Pattern/Level3/Inheritance/Problem2/Manager.java
@@ -1,1 +1,34 @@
+package Level3.Inheritance.Problem2;
 
+
+class Employee extends Person{
+    void display2(){
+    System.out.println("I am an Employee");
+}
+}
+
+
+
+class Person {
+    
+    void display1(){
+    System.out.println("I am a person");
+    }
+}
+
+
+
+
+public class Manager extends Employee {
+    void display(){
+        System.out.println("I am the manager.");
+    }
+    
+    
+    public static void main(String[] args) {
+        Manager m=new Manager();
+        m.display1();
+        m.display2();
+        m.display();
+    }
+}
