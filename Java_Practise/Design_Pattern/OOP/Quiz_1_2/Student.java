90371ed6e855ac06a997c4245c7f5bbbfa8e6785
Azizul Islam <azizulislam5360@gmail.com>
2026-02-22 16:37:55 +0600

    Add files via upload

--- Java_Practise/Design_Pattern/OOP/Quiz_1_2/Student.java
+++ Java_Practise/Design_Pattern/OOP/Quiz_1_2/Student.java
@@ -1,1 +1,32 @@
+package OOP.Quiz_1_2;
 
+public class Student {
+
+    private String name,id;
+    private double cgpa;
+    
+    Student(String name,String id,double cgpa){
+    this.name=name;
+    this.id=id;
+    this.cgpa=cgpa;        
+    }
+
+    String getName(){
+        return name;
+    }
+    String getId(){
+        return id;
+    }
+    double getcgpa(){
+        return cgpa;
+    }
+    
+}
+
+class StudentInfo {
+    public static void main(String[] args) {
+        Student s1=new Student("Azizul Islam", "241-35-364", 3.66);
+        System.out.println("");
+    }
+    
+}
