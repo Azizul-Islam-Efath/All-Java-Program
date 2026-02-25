43598ed628e5bd9464772edb9174959fb4d21bcc
Azizul Islam <azizulislam5360@gmail.com>
2026-02-25 09:30:46 +0600

    Add files via upload

--- Java_Practise/Design_Pattern/Level4/Access_Modifier/acc/Employeee.java
+++ Java_Practise/Design_Pattern/Level4/Access_Modifier/acc/Employeee.java
@@ -1,1 +1,21 @@
+package Level4.Access_Modifier.acc;
 
+import Level4.Access_Modifier.Person;
+
+public class Employeee extends Person{
+    
+    Employeee(String NID){
+        super(NID);
+    }
+
+    void display(){
+        System.out.println("The NID no of the person is : "+NID);
+    }
+
+    public static void main(String[] args) {
+        Employeee e1 = new Employeee("1237-4897-4479");
+        
+        e1.display();
+    
+    }
+}
