9e15b2d8fa8730951e48237e37dd14d73654f6bc
Azizul Islam <azizulislam5360@gmail.com>
2026-03-01 15:59:38 +0600

    Add files via upload

--- Java_Practise/Design_Pattern/Level4/Exception_Handling/Arithmatic.java
+++ Java_Practise/Design_Pattern/Level4/Exception_Handling/Arithmatic.java
@@ -1,1 +1,23 @@
+package Level4.Exception_handling;
 
+import java.util.Scanner;
+
+public class Arithmatic {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter two value: ");
+        int a =input.nextInt();
+        int b=input.nextInt();
+        input.close();
+
+        try{
+            int result = a/b;
+            System.out.println("Result : "+result);
+        }catch (ArithmeticException e){
+            System.out.println("Error : Any number cannot be devided by ZERO!");            
+        }
+        System.out.println("Program Run Continuously.");
+    }
+}
