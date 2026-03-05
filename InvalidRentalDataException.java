86960e16e778f81ef70988924cdda61feb6210f4
Azizul Islam <azizulislam5360@gmail.com>
2026-03-05 22:15:32 +0600

    Delete InvalidRentalDataException.java

--- InvalidRentalDataException.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.problem_solve_inher_inter_excep;
-
-public class InvalidRentalDataException extends RuntimeException {
-
-    InvalidRentalDataException(String m) {
-        super(m);
-    }
-}
