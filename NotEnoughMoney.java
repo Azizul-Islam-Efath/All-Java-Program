6e63418b96b9c8391e7793e8bd5efc571b037f2d
Azizul Islam <azizulislam5360@gmail.com>
2026-04-03 13:25:46 +0600

    Delete NotEnoughMoney.java

--- NotEnoughMoney.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.exception_handling;
-import java.util.*;
-class NotEnoughMoney extends Exception {
-    public NotEnoughMoney() {
-        super("Not enough money to withdraw.");
-    }
-}
