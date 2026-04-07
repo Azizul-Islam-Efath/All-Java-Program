ec612ba0d7ffc512c83272a7c00760990550be19
Azizul Islam <azizulislam5360@gmail.com>
2026-04-07 22:23:50 +0600

    Implement ItalicCommand for text formatting

---
+++ Java_Practise/Design_Pattern/Coursera/Command_Pattern/ItalicCommand.java
@@ -0,0 +1,12 @@
+class ItalicCommand implements Command {
+    private TextEditor editor;
+
+    public ItalicCommand(TextEditor editor) {
+        this.editor = editor;
+    }
+
+    @Override
+    public void execute() {
+        editor.italic();
+    }
+}
