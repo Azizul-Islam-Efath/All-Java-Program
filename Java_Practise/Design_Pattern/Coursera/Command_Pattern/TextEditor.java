5ddeecf2c2c84ef7ae670e4d720d825c9f2bda95
Azizul Islam <azizulislam5360@gmail.com>
2026-04-07 22:22:35 +0600

    Add TextEditor class with bold and italic methods

---
+++ Java_Practise/Design_Pattern/Coursera/Command_Pattern/TextEditor.java
@@ -0,0 +1,9 @@
+class TextEditor {
+    public void bold() {
+        System.out.println("Text is bold.");
+    }
+
+    public void italic() {
+        System.out.println("Text is italic.");
+    }
+}
